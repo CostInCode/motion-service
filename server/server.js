8cbe2fd2e6fee1a9fda596b3962ff3a423d15f40
costa <costmarullo@gmail.com>
2018-12-29 13:22:48 +0100

    Add get motions per hour

--- server/server.js
+++ server/server.js
@@ -30,8 +30,8 @@ app.get('/motions', (req, res) => {
 });
 
 
-// add new motion 
-app.post('/motions', (req, res) => {
+// add new motion // it is a get request because camera cant send post req
+app.get('/addMotion', (req, res) => {
 	const timestamp = Date.now();
 	const date = new Date(timestamp);
 	const year = date.getFullYear();
@@ -40,10 +40,10 @@ app.post('/motions', (req, res) => {
 	const hour = date.getUTCHours();
 	const minutes = date.getUTCMinutes();
 	new Motion({
-		message: req.body.message,
+		message: req.query.message,
 		year, month, day, hour, minutes
-	}).save().then((doc) => {
-		doc? res.send(doc) : res.status(400).send("bad request");
+	}).save().then((motion) => {
+		motion? res.send(motion) : res.status(400).send("bad request");
 	}).catch((e) => res.status(400).send(e));
 });
 
@@ -55,5 +55,30 @@ app.get('/motions/:hour', (req, res) => {
 	.catch((e) => res.sendStatus(400));
 });
 
+// given a day, get motions per hour 
+app.get('/motions/:year/:month/:day', (req, res) => {
+	Motion.find({
+		year: req.params.year,
+		month: req.params.month,
+		day: req.params.day
+	}, 'hour', (err, docs) => {
+		if(err) return;
+		let hours = [];
+		docs.forEach(doc => {
+			addOrUpdateHours(hours, doc);
+		});
+		res.send(`hours: ${JSON.stringify(hours)}`);
+	}).then(() => console.log('ok')).catch((e) => res.sendStatus(400));
+});
+
+const addOrUpdateHours = (array, item) => {
+	const i = array.findIndex(_item => _item.h === item.hour);
+	if(i > -1) {
+		let c = array[i].count + 1;
+		array[i] = {h: item.hour, count: c};
+	} 
+	else array.push({h: item.hour, count: 1});
+}
+	
 
 module.exports = {app};
